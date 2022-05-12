27d480b0d7e9f44b6ee27073a95a8757a4eb03f4
Enzo VALENTIN <Valentinenzo@outlook.fr>
2022-05-12 23:04:18 +0200

    Erreur hasher

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -69,7 +69,7 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
     ],
     paginationEnabled: false,
 )]
-class User implements UserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
